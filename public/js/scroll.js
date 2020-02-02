8cac37407e9f1451187a90df1c30bc0e5495e8b3
Hemant Singh <singh.hemant9583@gmail.com>
2020-02-02 20:37:05 +0530

    fourth commit home page abhinandan

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -5,3 +5,51 @@ function myFunction() {
   var scrolled = (winScroll / height) * 100;
   document.getElementById("myBar").style.width = scrolled + "%";
 }
+$(window).resize(function() {
+  if ($(this).width() < 1024) {
+    $('.size_hide').hide();
+  } else {
+    $('.size_hide').show();
+    }
+});
+
+var textWrapper = document.querySelector('.ml7 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml7 .letter',
+    translateY: ["1.1em", 0],
+    translateX: ["0.55em", 0],
+    translateZ: 0,
+    rotateZ: [180, 0],
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: (el, i) => 50 * i
+  });
+  
+  $('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function(event) {
+      event.preventDefault()
+        if (
+          location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+          &&
+          location.hostname == this.hostname
+        ) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+          if (target.length) {
+            event.preventDefault();
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, 1000, function() {
+              var $target = $(target);
+              $target.focus();
+              if ($target.is(":focus")) {
+                return false;
+              } else {
+                $target.attr('tabindex','-1');
+                $target.focus();
+              };
+            });
+          }
+        }
+      });
